b21a1e13a1d1d75004097066281175ed31d67d88
Luiza Tanasie <luiza.tanasie@qubiz.com>
2020-10-18 17:39:57 +0300

    Add project files.

---
+++ SmartGarden/Models/SmartPotModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartGarden.Models
+{
+    public class SmartPotModel
+    {
+        [JsonProperty(PropertyName = "temperature")]
+        public decimal Temperature { get; set; }
+        [JsonProperty(PropertyName = "humidity")]
+        public decimal Humidity { get; set; }
+        [JsonProperty(PropertyName = "soil_moisture")]
+        public decimal SoilMoisture { get; set; }
+        [JsonProperty(PropertyName = "is_raining")]
+        public bool IsRaining { get; set; }
+    }
+}
